5b44560088ba25ec592fd60b09b68a2624532ad7
John serrano <andrey_201015@hotmail.com>
2018-02-06 17:49:20 -0500

    Profile styles

--- sites/default/themes/ads/user-profile.tpl.php
+++ sites/default/themes/ads/user-profile.tpl.php
@@ -1,40 +1,9 @@
 <section id="profile" class="container">
   <article id="profileInfo">
     <hr>
-    <h3><?php print render($user_profile['field_tipo']) ?></h3>
     <article class="descripcion">
-      <div class="top"></div>
-      <div class="left"></div>
       <div class="logo"><?php print render($user_profile['user_picture']) ?></div>
-      <p><?php print render($user_profile['field_descripcion']) ?></p>
-      <div class="right"></div>
+      <?php print render($user_profile); ?>
     </article>
   </article>
-  
-  <article id="proyectos">
-    <aside class="triangulo"></aside>
-    <aside class="estrellas"></aside>
-    <h3>Proyectos</h3>
-    <?php $view = views_get_view('profiles'); print $view->preview('block_3'); ?>
-  </article>
-
-  <article id="anuncios-1">
-    <hr>
-    <?php $view = views_get_view('profiles'); print $view->preview('block'); ?>
-  </article>
-
-  <div class="pauta"></div>
-
-  <article id="anuncios-2">
-    <hr>
-    <?php $view = views_get_view('profiles'); print $view->preview('block_1'); ?>
-  </article>
-
-  <article id="otrosAnuncios">
-    <hr>
-    <aside class="estrellas"></aside>
-    <h3>También te puede interesar</h3>
-    <?php $view = views_get_view('profiles'); print $view->preview('block_2'); ?>
-  </article>
-
 </section>
